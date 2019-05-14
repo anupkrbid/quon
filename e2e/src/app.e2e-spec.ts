f09d3fc3ec1b4721b0914c0230bcb6ad96d4b8b4
anupkrbid <anupbid94@gmail.com>
2019-05-15 03:03:20 +0530

    Updated project

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to socal-media-handler!');
+    expect(page.getTitleText()).toEqual('Welcome to quon!');
   });
 
   afterEach(async () => {
